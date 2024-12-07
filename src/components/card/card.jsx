347829b052a9fc929a355c5791c939c62d44e0a5
NilufarSalimova2000 <nilufarsalimova2000@gmail.com>
2024-12-07 20:17:36 +0500

    add

--- src/components/card/card.jsx
+++ src/components/card/card.jsx
@@ -5,7 +5,7 @@ import { useDispatch } from "react-redux";
 export const Card = ({ id, title }) => {
     const dispatch = useDispatch();
     return (
-        <div className="w-[400px] rounded border border-[2px] border-pink-300 p-[20px] mb-[20px]">
+        <div className="w-[400px] rounded border-[2px] border-pink-300 p-[20px] mb-[20px]">
             <h1 className="text-center mb-[20px] font-semibold text-pink-300 text-[25px]">{title}</h1>
             <div className="flex justify-center gap-[15px]">
                 <button className="add_btn rounded bg-pink-400 text-[#fff] px-[15px] py-[11px]" onClick={() => dispatch(removeTask(id))}>Delete</button>
